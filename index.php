2b6f87c9ede8666c7ed6bf1304418f214a6947c4
wahyukusuma24 <wahyukusuma1521@gmail.com>
2020-11-11 00:04:52 +0800

    menambahkan file navbar dan gambar

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="src/css/indx.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="logo">
+        <img src="image/indeximg/logo.png" alt="">
+    </div>
+    <div class="flex-container">
+        <div><a href=""><img src="image/indeximg/bgman.jpg" alt=""></a></div>
+        <div><a href=""><img src="image/indeximg/bgwoman.jpg" alt=""></a></div>
+    </div>
+</body>
+</html>
